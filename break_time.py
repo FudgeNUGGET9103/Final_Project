632be19adf9c88d1aeb2e204a139f2c6841f6030
G-Money <gaewiltshire@gmail.com>
2021-08-06 11:47:21 -0500

    Tried to make class didnt work so this is rough but integrates file function

--- break_time.py
+++ break_time.py
@@ -13,6 +13,41 @@ if answer == "yes":
     path = os.path.join(parent_dir, filename)
     os.mkdir(path)
     del os
+    break_count = 0
+    total_break = 2
+    while (break_count < total_break):
+        time.sleep(20)
+        root = tkinter.Tk()
+        root.title("Would you like to take a picture?")
+        root.geometry('500x300')        
+        print("this program started on" + time.ctime())
+        PosNeg = []
+    
+        def yesClick():
+            tkinter.messagebox.showinfo("!",  "Say Cheese")
+            import cv2
+            vid = cv2.VideoCapture(0)
+            ret, frame = vid.read()
+            while(True):
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.imwrite('{}/{}.jpg'.format(filename, date.today()),frame)
+                    cv2.destroyAllWindows()
+                    break
+            vid.release()
+            del cv2
+    
+        def noClick():
+            tkinter.messagebox.showinfo("Alright, no problem",  "See you in 10 days!")
+    
+        Button1 = Button(root, activebackground="blue", command=lambda:[yesClick(), PosNeg.append(1)], text="Yes",  height=5, width=10)
+        Button2 = Button(root, activebackground="red", command=lambda:[noClick(), PosNeg.append(0)], text="No", height=5, width=10)
+        Button1.pack(side=LEFT)
+        Button2.pack(side=RIGHT)
+        root.mainloop()
+        time.sleep(4)
+        break_count = break_count + 1
+
 elif answer == "no":
     import os
     print("What existing folder would you like to use?")
@@ -21,38 +56,37 @@ elif answer == "no":
     path = os.path.join(parent_dir, filename)
     os.mkdir(path)
     del os
-
-break_count = 0
-total_break = 2
-while (break_count < total_break):
-    time.sleep(20)
-    root = tkinter.Tk()
-    root.title("Would you like to take a picture?")
-    root.geometry('500x300')        
-    print("this program started on" + time.ctime())
-    PosNeg = []
+    break_count = 0
+    total_break = 2
+    while (break_count < total_break):
+        time.sleep(20)
+        root = tkinter.Tk()
+        root.title("Would you like to take a picture?")
+        root.geometry('500x300')        
+        print("this program started on" + time.ctime())
+        PosNeg = []
     
-    def yesClick():
-        tkinter.messagebox.showinfo("!",  "Say Cheese")
-        import cv2
-        vid = cv2.VideoCapture(0)
-        ret, frame = vid.read()
-        while(True):
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                cv2.imwrite('Desktop/{}.jpg'.format(date.today()),frame)
-                cv2.destroyAllWindows()
-                break
-        vid.release()
-        del cv2
+        def yesClick():
+            tkinter.messagebox.showinfo("!",  "Say Cheese")
+            import cv2
+            vid = cv2.VideoCapture(0)
+            ret, frame = vid.read()
+            while(True):
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.imwrite('Desktop/{}.jpg'.format(date.today()),frame)
+                    cv2.destroyAllWindows()
+                    break
+            vid.release()
+            del cv2
     
-    def noClick():
-        tkinter.messagebox.showinfo("Alright, no problem",  "See you in 10 days!")
+        def noClick():
+            tkinter.messagebox.showinfo("Alright, no problem",  "See you in 10 days!")
     
-    Button1 = Button(root, activebackground="blue", command=lambda:[yesClick(), PosNeg.append(1)], text="Yes",  height=5, width=10)
-    Button2 = Button(root, activebackground="red", command=lambda:[noClick(), PosNeg.append(0)], text="No", height=5, width=10)
-    Button1.pack(side=LEFT)
-    Button2.pack(side=RIGHT)
-    root.mainloop()
-    time.sleep(4)
-    break_count = break_count + 1
+        Button1 = Button(root, activebackground="blue", command=lambda:[yesClick(), PosNeg.append(1)], text="Yes",  height=5, width=10)
+        Button2 = Button(root, activebackground="red", command=lambda:[noClick(), PosNeg.append(0)], text="No", height=5, width=10)
+        Button1.pack(side=LEFT)
+        Button2.pack(side=RIGHT)
+        root.mainloop()
+        time.sleep(4)
+        break_count = break_count + 1
